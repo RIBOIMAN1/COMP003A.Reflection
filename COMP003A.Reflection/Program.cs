bd97266500e82de0a26f8a7377939c7551dc6991
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-02-24 04:01:23 -0800

    Answer the seven questions found on the 7.2 coding assignment.

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			Console.WriteLine("1. What worked well for you over the last 8 weeks?");
+			Console.WriteLine("For me, the things that worked the best over the last 8 weeks were the ability to not have to rely on a textbook for this course, as it was easier to follow our curriculum; and I also like how you did code examples in class, showing stuff on both your whiteboard and with external displays across the classroom.");
+			Console.WriteLine("\n2. What external resources did you use to aid your learning?");
+			Console.WriteLine("The external resources that I used throughout this course to better understand what I was learning where web sources are found during discussion assignments. The sources typically varied every assignment, but I remember Microsoft giving really good documentation on C# code.");
+			Console.WriteLine("\n3. What would you change about the course?");
+			Console.WriteLine("I would personally like to see More leniency on the due dates for assignments. I have always felt like I was on a time crunch every single week doing your assignments, whereas with front end development, I was able to get things done relatively comfortably with a Sunday due date across all of my assignments.");
+			Console.WriteLine("\n4. Did you learn what you expected to learn in this course?");
+			Console.WriteLine("Yes, I did feel that I have learned what I expected to in this course.");
+			Console.WriteLine("\n5. What concepts were new to you?");
+			Console.WriteLine("Object-oriented programming, switch statements (though similar to if statements and its derivatives which I do have some experience with in other coding languages), and the many different types of classes that you could have in a program along with the different things that you could do with them are among some of the new things that I have had a new learning experience with during this course.");
+			Console.WriteLine("\n6. What concepts have you encountered already in the past?");
+			Console.WriteLine("I have encountered if statements and its derivatives as well as arithmetic expressions in coding before taking this class.");
+			Console.WriteLine("\n7. Do you have any tips/comments for the instructor?");
+			Console.WriteLine("As previously stated in question #3, I would like the due dates to be a little bit more lenient. Sunday due dates for everything we did worked fine when we were in front end development, and I felt like it was easier for me to get stuff done both in this class and in my own classes.");
+		}
     }
 }
